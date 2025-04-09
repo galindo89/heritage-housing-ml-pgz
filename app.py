a520b819dfad4f5acc28e77b4a5b501612c8edb7
Pablo <pablo@livedooh.com>
2025-04-10 00:02:02 +0200

    adding structure of summary page. Testing sreamlit

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 print  ("Starting the app...")
+
+# Import all pages scripts
+from app_pages.page_summary import page_summary_body
+
+# Create an instance of the MultiPage class
+app = MultiPage("Inherited House Price Prediction App")
+
+# Add pages to the app
+app.add_page("Project Summary", page_summary_body)
+
+
+# Run the app
+app.run()
---
+++ app_pages/page_summary.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def page_summary_body():
+    st.title("Summary")
+    st.write("This is the summary page.")
+   
